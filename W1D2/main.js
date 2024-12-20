bd8a0b4a3e7b3683fd2019372a5d8b7f8b7a27e4
Mattia-Chiccoli <mattiachiccoli00@gmail.com>
2024-12-20 19:52:51 +0100

    Upload esercizi day1, day2 e day 4.

---
+++ W1D2/main.js
@@ -0,0 +1,10 @@
+let age = 18;
+let drivingLicense = false;
+
+if (age >= 18 && drivingLicense == true) {
+  console.log("Puoi guidare!");
+} else if (age >= 18 && drivingLicense == false) {
+  console.log("Non puoi guidare, non hai la patente!");
+} else {
+  console.log("Non puoi guidare, non sei maggiorenne!");
+}
